e77fae118f20c5bb23d8858ed2ad9cd74a03aa08
marcellmueller <mlmueller@protonmail.com>
2020-09-25 13:20:37 -0700

    began adding function to parse options on chart init

--- src/options.js
+++ src/options.js
@@ -3,7 +3,14 @@ export let options = {
   labelColor: "#f7f7f7",
   barSpacing: "5px",
   chartAxis: "horizontal",
-  barLabelPosition: "bottom",
-  titleFontSize: "30px",
+  barLabelPosition: "right",
+  titleFontSize: 30,
   titleFontColor: "#f7f7f7",
+  titleBackgroundColor: "#f7f7f7",
+};
+
+export const initOptions = (options) => {
+  let title = document.getElementById("barChartTitle");
+  title.style.fontSize = options.titleFontSize + "px";
+  title.style.backgroundColor = options.titleBackgroundColor;
 };
--- src/render.js
+++ src/render.js
@@ -1,17 +1,19 @@
 import { barRangeConvert } from "./convert.js";
-import { options } from "./options.js";
+import { options, initOptions } from "./options.js";
 //rendering to DOM related code in here
 
 export const renderChart = (numbers, numbersArr) => {
+  initOptions(options);
   for (let i = 0; i < numbers.length; i++) {
     let range = barRangeConvert(numbers, numbers[i]);
     let barChart = document.getElementById("barChart");
     let bar = document.createElement("div");
     bar.classList.add("bars");
-    bar.setAttribute("id", numbers[i]);
 
+    bar.style.color = options.labelColor;
     bar.style.backgroundColor = barColor(numbersArr, i, options);
     bar.style.width = range + "%";
+    bar.style.textAlign = options.barLabelPosition;
     bar.innerHTML = numbers[i];
     barChart.appendChild(bar);
   }
